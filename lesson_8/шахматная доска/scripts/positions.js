67c4c71f2eba23aef37586dc21c95205a72ac5fb
Vilenan <vilenan@mail.ru>
2021-02-15 13:10:15 +0300

    шахматная доска

--- lesson_8/шахматная доска/scripts/positions.js
+++ lesson_8/шахматная доска/scripts/positions.js
@@ -3,48 +3,56 @@ let positions = [
     {
         coordCol: 'a',
         coordRow: 1,
+        code: '&#9814',
         figure: 'rook',
         color: 'white'
     },
     {
         coordCol: 'b',
         coordRow: 1,
+        code: '&#9816',
         figure: 'knight',
         color: 'white'
     },
     {
         coordCol: 'c',
         coordRow: 1,
+        code: '&#9815',
         figure: 'bishop',
         color: 'white'
     },
     {
         coordCol: 'd',
         coordRow: 1,
+        code: '&#9813',
         figure: 'queen',
         color: 'white'
     },
     {
         coordCol: 'e',
         coordRow: 1,
+        code: '&#9812',
         figure: 'king',
         color: 'white'
     },
     {
         coordCol: 'f',
         coordRow: 1,
+        code: '&#9815',
         figure: 'bishop',
         color: 'white'
     },
     {
         coordCol: 'g',
         coordRow: 1,
+        code: '&#9816',
         figure: 'knight',
         color: 'white'
     },
     {
         coordCol: 'h',
         coordRow: 1,
+        code: '&#9814',
         figure: 'rook',
         color: 'white'
     },
@@ -56,48 +64,57 @@ let positions = [
     {
         coordCol: 'a',
         coordRow: 8,
+        code: '&#9820',
         figure: 'rook',
         color: 'black'
+        
     },
     {
         coordCol: 'b',
         coordRow: 8,
+        code: '&#9822',
         figure: 'knight',
         color: 'black'
     },
     {
         coordCol: 'c',
         coordRow: 8,
+        code: '&#9821',
         figure: 'bishop',
         color: 'black'
     },
     {
         coordCol: 'd',
         coordRow: 8,
+        code: '&#9819',
         figure: 'queen',
         color: 'black'
     },
     {
         coordCol: 'e',
         coordRow: 8,
+        code: '&#9818',
         figure: 'king',
         color: 'black'
     },
     {
         coordCol: 'f',
         coordRow: 8,
+        code: '&#9821',
         figure: 'bishop',
         color: 'black'
     },
     {
         coordCol: 'g',
         coordRow: 8,
+        code: '&#9822',
         figure: 'knight',
         color: 'black'
     },
     {
         coordCol: 'h',
         coordRow: 8,
+        code: '&#9820',
         figure: 'rook',
         color: 'black'
     },
